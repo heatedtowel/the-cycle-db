f7469ca4158ce3c412fde31d4ea1fc18fbabe91e
Jesse Monks <monksjesse@gmail.com>
2022-08-06 02:48:44 -0400

    style changes, change to backpack methods and fixed error when selecting backpack rarity.

--- src/App.js
+++ src/App.js
@@ -5,25 +5,12 @@ import newdb from './newdb.json'
 import './App.css';
 
 function App() {
+  var categories = ['Armor', 'Utilities'];
+
   let [category, setCategory] = useState('')
-  let [armorPiece, setArmorPiece] = useState(null)
   let [rarity, setRarity] = useState(null)
+  let [armorPiece, setArmorPiece] = useState(null)
   let [armorType, setArmorType] = useState(null)
-
-
-  const filter = () => {
-
-    let filteredRarity = newdb['Utilities']["backpack"].filter((el) => el.rarity === 'rare')
-
-    console.log(filteredRarity[0].rarity)
-
-    if (filteredRarity['Crafting Materials']) {
-      console.log('true')
-    }
-
-  }
-
-  filter()
 
   const categoryChange = (value) => {
     setCategory(value)
@@ -47,134 +34,74 @@ function App() {
         <div className='main--container'>
           <div className='item--outer--container'>
             <div>
-              <h4>Choose a Category:</h4>
-              <div className='btn--container'>
-                {Object.keys(newdb).map((value) =>
-                  <button
-                    onClick={() => categoryChange(value)}
-                  >{value}
-                  </button>
-                )}
-              </div>
-            </div>
-
-            {(category) &&
               <div>
-                <h4>{category}</h4>
+                <h4>Category</h4>
                 <div className='btn--container'>
-                  {category && Object.keys(newdb[category]).map((value) =>
-                    <button onClick={() => setArmorPiece(value)}>{value}</button>)}
+                  {categories.map((category) =>
+                    <button
+                      onClick={() => categoryChange(category)}
+                    >{category}
+                    </button>
+                  )}
                 </div>
               </div>
-            }
 
-            {(category && armorPiece) &&
-              <div>
-                <h4>Rarity:</h4>
-                <div className='btn--container'>
-                  {armorPiece && Object.keys(newdb[category][armorPiece]).map((value) => <button onClick={() => armorRarityBtn(value)}>{value}</button>)}
-                </div>
-              </div>
-            }
-
-            {(category === 'Armor' && armorPiece && rarity) &&
-              <div>
-                <h4>Armor Type</h4>
-                <div className='btn--container'>
-                  {(armorPiece && rarity) && Object.keys(cycleDB[category][armorPiece][rarity]).map((value) => <button onClick={() => setArmorType(value)
-                  }>{value}</button>)}
-                </div>
-              </div>
-            }
-
-            {(category && armorPiece && armorType && rarity) &&
-              <div className='item--container'>
-                {Object.keys(cycleDB[category][armorPiece][rarity][armorType]).map(components =>
-                  <div className='item'>
-                    <h4>{components}: {cycleDB[category][armorPiece][rarity][armorType][components]}</h4>
+              {(category) &&
+                <div>
+                  <h4>{category}</h4>
+                  <div className='btn--container'>
+                    {category && Object.keys(newdb[category]).map((value) =>
+                      <button onClick={() => setArmorPiece(value)}>{value}</button>)}
                   </div>
-                )}
-              </div>}
+                </div>
+              }
+
+              {(category && armorPiece) &&
+                <div>
+                  <h4>Rarity</h4>
+                  <div className='btn--container'>
+                    {armorPiece && Object.keys(newdb[category][armorPiece]).map((value) => <button onClick={() => armorRarityBtn(value)}>{value}</button>)}
+                  </div>
+                </div>
+              }
+
+              {(category === 'Armor' && armorPiece && rarity) &&
+                <div>
+                  <h4>Armor Type</h4>
+                  <div className='btn--container'>
+                    {(armorPiece && rarity) && Object.keys(cycleDB[category][armorPiece][rarity]).map((value) => <button onClick={() => setArmorType(value)
+                    }>{value}</button>)}
+                  </div>
+                </div>
+              }
+
+              {(category && armorPiece && armorType && rarity) &&
+                <div className='item--container'>
+                  <h2>Materials Needed</h2>
+                  {Object.keys(cycleDB[category][armorPiece][rarity][armorType]).map(components =>
+                    <div className='item'>
+                      <h4>{components}: {cycleDB[category][armorPiece][rarity][armorType][components]}</h4>
+                    </div>
+                  )}
+                </div>
+              }
+            </div>
             {(category && armorPiece === 'backpack' && rarity) &&
               <div className='item--container'>
+                <img src={newdb[category][armorPiece][rarity]["image"]} alt="" />
+                <h3>Materials Needed</h3>
                 {Object.keys(cycleDB[category][armorPiece][rarity]).map(components =>
                   <div className='item'>
-                    <h4 className='item--name'>{components}:</h4>
-                    <h4 className='item--ammount'>{cycleDB[category][armorPiece][rarity][components]}</h4>
+                    <h4>{components}: {cycleDB[category][armorPiece][rarity][components]}</h4>
                   </div>
                 )}
               </div>
             }
           </div>
-          <OreSpawns data={newdb.Mining} />
+          <OreSpawns Mining={newdb.Mining} />
         </div>
       </header>
     </div >
-    // <div className="App">
-    //   <header className="App-header">
-    //     <div className='main--container'>
-    //       <div>
-    //         <div>
-    //           <h4>Choose a Category:</h4>
-    //           <div className='btn--container'>
-    //             {Object.keys(cycleDB).map((value) =>
-    //               <button
-    //                 onClick={() => categoryChange(value)}
-    //               >{value}
-    //               </button>
-    //             )}
-    //           </div>
-    //         </div>
-
-    //         {(category) &&
-    //           <div>
-    //             <h4>{category}</h4>
-    //             <div className='btn--container'>
-    //               {category && Object.keys(cycleDB[category]).map((value) =>
-    //                 <button onClick={() => setArmorPiece(value)}>{value}</button>)}
-    //             </div>
-    //           </div>
-    //         }
-
-    //         {(category && armorPiece) &&
-    //           <div>
-    //             <h4>Rarity:</h4>
-    //             <div className='btn--container'>
-    //               {armorPiece && Object.keys(cycleDB[category][armorPiece]).map((value) => <button onClick={() => armorRarityBtn(value)}>{value}</button>)}
-    //             </div>
-    //           </div>
-    //         }
-
-    //         {(category === 'Armor' && armorPiece && rarity) &&
-    //           <div>
-    //             <h4>Armor Type</h4>
-    //             <div className='btn--container'>
-    //               {(armorPiece && rarity) && Object.keys(cycleDB[category][armorPiece][rarity]).map((value) => <button onClick={() => setArmorType(value)
-    //               }>{value}</button>)}
-    //             </div>
-    //           </div>
-    //         }
-    //       </div>
-    //       {(category && armorPiece && armorType && rarity) &&
-    //         <div className='item--container'>
-    //           {Object.keys(cycleDB[category][armorPiece][rarity][armorType]).map(components =>
-    //             <div className='item'>
-    //               <h4>{components}: {cycleDB[category][armorPiece][rarity][armorType][components]}</h4>
-    //             </div>
-    //           )}
-    //         </div>}
-    //       {(category && armorPiece === 'backpack' && rarity) &&
-    //         <div className='item--container'>
-    //           {Object.keys(cycleDB[category][armorPiece][rarity]).map(components =>
-    //             <div className='item'>
-    //               <h4 className='item--name'>{components}:</h4>
-    //               <h4 className='item--ammount'>{cycleDB[category][armorPiece][rarity][components]}</h4>
-    //             </div>
-    //           )}
-    //         </div>}
-    //     </div>
-    //   </header>
-    // </div >
   );
 }
 
--- src/components/oreSpawns/OreSpawns.js
+++ src/components/oreSpawns/OreSpawns.js
@@ -1,19 +1,31 @@
 import React from 'react'
 import './oreSpawns.css'
 
-const OreSpawns = ({ data }) => {
+const OreSpawns = ({ Mining }) => {
 
   return (
     <div className='ore--outer--container'>
-      {Object.keys(data).map(ore =>
+      {Object.keys(Mining).map(ore =>
         <div className='ore--container'>
           <div className='ore--spawn--container'>
             <h4 className='ore--name'>{ore}:</h4>
-            <h4 className='ore--rates--container'>{Object.keys(data[ore]["Spawn Rates"]).map(rate => <h5 className='ore--rates'>{rate}: {data[ore]["Spawn Rates"][rate]}</h5>)}</h4>
+            <div className='ore--rates--container'>
+              {Object.keys(Mining[ore]["Spawn Rates"]).map(rate =>
+                <div className='ore--rates--inner--container'>
+                  <h5 className='ore--rates--name'>{rate}</h5>
+                  <h5 className='ore--rates--amount'>{Mining[ore]["Spawn Rates"][rate]}</h5>
+                </div>
+              )}
+            </div>
           </div>
           <div className='ore--location--container'>
             <h4 className='ore--name'>Locations:</h4>
-            <h4 className='ore--locations--container'>{Object.keys(data[ore]["Spawn Locations"]).map(location => <h5 className='ore--locations'>{location}: {data[ore]["Spawn Locations"][location]}</h5>)}</h4>
+            <div className='ore--locations--container'>{Object.keys(Mining[ore]["Spawn Locations"]).map(location =>
+              <div className='ore--locations--inner--container'>
+                <h5 className='ore--locations--name'>{location}</h5>
+                <h5 className='ore--locations--amount'>{Mining[ore]["Spawn Locations"][location]}</h5>
+              </div>)}
+            </div>
           </div>
         </div>
       )}
