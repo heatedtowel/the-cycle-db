07a079a7349979e693e589604dd81135f724752e
Jesse Monks <monksjesse@gmail.com>
2022-08-22 00:04:08 -0400

    refactoring, style changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 function App() {
   var categories = ['Armor', 'Utilities'];
 
-  let [category, setCategory] = useState('')
+  let [category, setCategory] = useState('Armor')
   let [armorPiece, setArmorPiece] = useState(null)
   let [rarity, setRarity] = useState(null)
   let [armorType, setArmorType] = useState(null)
@@ -48,42 +48,39 @@ function App() {
                 </div>
               </div>
 
-              {(category) &&
-                <div>
-                  <h4 className='category--title'>{category}</h4>
-                  <div className='btn--container'>
-                    <select name="category" id="category-select" onChange={(e) => setArmorPiece(e.target.value)}>
-                      <option selected value="">----</option>
-                      {category && Object.keys(newdb[category]).map((value) =>
-                        <option
-                          value={value}
-                        >{value}
-                        </option>
-                      )}
-                    </select>
-                  </div>
+              <div className='dropdown--section'>
+                <h4 className='category--title'>{category}</h4>
+                <div className='btn--container'>
+                  <select name="category" id="category-select" onChange={(e) => setArmorPiece(e.target.value)}>
+                    <option selected value="">----</option>
+                    {category && Object.keys(newdb[category]).map((value) =>
+                      <option
+                        value={value}
+                      >{value}
+                      </option>
+                    )}
+                  </select>
                 </div>
-              }
+              </div>
 
-              {(category && armorPiece) &&
-                <div>
-                  <h4 className='rarity--title'>Rarity</h4>
-                  <div className='btn--container'>
-                    <select name="category" id="category-select" onChange={(e) => categoryButton(e.target.value)}>
-                      <option selected value="">----</option>
-                      {armorPiece && Object.keys(newdb[category][armorPiece]).map((value) =>
-                        <option
-                          value={value}
-                        >{value}
-                        </option>
-                      )}
-                    </select>
-                  </div>
+
+              <div className='dropdown--section'>
+                <h4 className='rarity--title'>Rarity</h4>
+                <div className='btn--container'>
+                  <select name="category" id="category-select" onChange={(e) => categoryButton(e.target.value)}>
+                    <option selected value="">----</option>
+                    {armorPiece && Object.keys(newdb[category][armorPiece]).map((value) =>
+                      <option
+                        value={value}
+                      >{value}
+                      </option>
+                    )}
+                  </select>
                 </div>
-              }
+              </div>
 
               {(category === 'Armor' && armorPiece && rarity) &&
-                <div>
+                <div className='dropdown--section'>
                   <h4 className='armor--type--title'>Armor Type</h4>
                   <div className='btn--container'>
                     <select name="category" id="category-select" onChange={(e) => setArmorType(e.target.value)}>
